367807b2094bb2d9483411ef336ca049c36e34a5
shyimon <botticelli.cosimo@gmail.com>
2024-11-21 16:17:01 +0100

    Added time period to API request

--- kpi_engine/main.py
+++ kpi_engine/main.py
@@ -1,6 +1,15 @@
 from kpi_calculation import kpi_engine
 from fastapi import FastAPI, HTTPException
 import pandas as pd
+from typing import Optional
+
+''''
+Start by using 'uvicorn main:app --reload'
+
+Examples of basic queries:
+GET http://127.0.0.1:8000/kpi/power_consumption_efficiency/calculate?machineId=all_machines (expected value =~ 357.414,5325)
+GET http://127.0.0.1:8000/kpi/availability/calculate?machineId=ast-hnsa8phk2nay (expected value =~ 0,9799)
+'''
 
 pd.set_option('display.max_rows', None)
 
@@ -18,9 +27,12 @@ async def read_root():
 async def calculate(
     kpiID: str,
     machineId: str,
-    # startTime: str,
-    # endTime: str
+    startTime: str,
+    endTime: str
+    # startTime: Optional[str] = "0",
+    # endTime: Optional[str] = "3"
     ):
+    print(f"Received kpiID: {kpiID}, machineId: {machineId}, startTime: {startTime}, endTime: {endTime}")
     methods = {
     name: getattr(kpi_engine, name)
     for name in dir(kpi_engine)
@@ -29,14 +41,13 @@ async def calculate(
     if kpiID not in methods:
         raise HTTPException(status_code=404, detail=f"Method for calculating '{kpiID}' not found")
 
-    result = methods[kpiID](df = df, machine_id = machineId, start_time = '1', end_time = '3')
+    result = methods[kpiID](df = df, machine_id = machineId, start_time = startTime, end_time = endTime)
     return {"value": result}
 
 def main_test():
     kpi_engine.dynamic_kpi(df=df, machine_id='all_machines', machine_type='any', start_time='2024-08-27T00:00:00Z', end_time='2024-09-20T00:00:00Z', kpi_id='a')
+
 if __name__ == "__main__":
-    main_test()
-    '''
     import uvicorn
     uvicorn.run(app, host="127.0.0.1", port=8000, reload=True)
-    '''
+    
