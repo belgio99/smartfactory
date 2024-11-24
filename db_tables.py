44c04bf0cd36bfdf98ed6fbfd51852ceb948a896
Emiliano <57751761+Faxatos@users.noreply.github.com>
2024-11-24 18:35:36 +0100

    Fixed db_tables.py typo

--- db_tables.py
+++ db_tables.py
@@ -37,7 +37,7 @@ if __name__ == "__main__":
             Role VARCHAR(20) NOT NULL,
             Password VARCHAR(50) NOT NULL,
             SiteName VARCHAR(50) NOT NULL,
-            UserSettings TEXT
+            UserSettings TEXT,
             UserDashboards TEXT
             )
             """,
