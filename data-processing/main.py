349e0b776e49b36f0d98ed4775a38ec63017ed96
Davide Egidio Cerioli <Ceriolide@gmail.com>
2024-11-29 18:44:11 +0100

    First integration updates
    First integration updates

--- data-processing/main.py
+++ data-processing/main.py
@@ -1,8 +1,29 @@
 import f_CharacterizeKPI
 import f_forecasting
+import uvicorn
 
-m = 'Medium Capacity Cutting Machine 1'
-k = 'consumption'
-x,y = data_load(m,k)
-plt.plot(x,y)
-characterize_KPI('Medium Capacity Cutting Machine 1', 'consumption')
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi import FastAPI
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:8080"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/data-processing")
+def fun():
+    return {'hello': 5}
+
+if __name__ == "__main__":
+    uvicorn.run(app, port=8000, host="0.0.0.0") # potrebbe essere bloccante
+
+    m = 'Medium Capacity Cutting Machine 1'
+    k = 'consumption'
+    x,y = data_load(m,k)
+    plt.plot(x,y)
+    characterize_KPI('Medium Capacity Cutting Machine 1', 'consumption')
