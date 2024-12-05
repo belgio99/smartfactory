42259d559aeee826b84f3ed463641a13a0270f55
shyimon <botticelli.cosimo@gmail.com>
2024-12-05 15:48:40 +0100

    JSON Array implemented but mock database readded

--- kpi-engine/src/main.py
+++ kpi-engine/src/main.py
@@ -1,10 +1,11 @@
 from kpi_calculation import kpi_engine
 from fastapi import FastAPI, HTTPException
 import pandas as pd
+from typing import List
 import uvicorn
 import requests
 from pydantic import BaseModel
-import os
+import os, time
 from pathlib import Path
 from dotenv import load_dotenv
 from fastapi.middleware.cors import CORSMiddleware
@@ -16,6 +17,10 @@ env_path = Path(__file__).resolve().parent.parent / ".env"
 print(env_path)
 load_dotenv(dotenv_path=env_path)
 
+with open("../smart_app_data.pkl", "rb") as file:
+    df = pd.read_pickle(file)
+
+'''
 headers = {
     "Content-Type": "application/json",
     "x-kpi-key": "b3ebe1bb-a4e7-41a3-bbcc-6c281136e234"
@@ -30,9 +35,9 @@ try:
     df = response.json()  # Return the JSON response
 except requests.exceptions.RequestException as e:
     print(f"An error occurred: {e}")
-    return
+    exit()
 df = pd.DataFrame.from_dict(df, orient='columns')
-
+'''
 app = FastAPI()
 
 app.add_middleware(
@@ -48,7 +53,7 @@ class KPIRequest(BaseModel):
     Machine_Name: Optional[str] = "all_machines"
     Machine_Type: Optional[str] = "any"
     Date_Start: Optional[str] = df['time'].min()[:10]
-    Date_End: Optional[str] = df['time'].max()[:10]
+    Date_Finish: Optional[str] = df['time'].max()[:10]
     Aggregator: Optional[str] = 'sum'
     startPreviousPeriod: Optional[str] = df['time'].min()[:10]
     endPreviousPeriod: Optional[str] = df['time'].max()[:10]
@@ -58,18 +63,9 @@ async def read_root():
     return {"message": "Welcome to the KPI Calculation Engine!"}
 
 @app.post("/kpi/calculate")
-async def calculate(request: KPIRequest, api_key: str = Depends(get_verify_api_key(["gui"]))): # to add or modify the services allowed to access the API, add or remove them from the list in the get_verify_api_key function e.g. get_verify_api_key(["gui", "service1", "service2"])
+# api_key: str = Depends(get_verify_api_key(["gui", "data"]))
+async def calculate(request: List[KPIRequest]): # to add or modify the services allowed to access the API, add or remove them from the list in the get_verify_api_key function e.g. get_verify_api_key(["gui", "service1", "service2"])
     ''' print(f"Received request: {request.json()}") '''
-
-    kpiID = request.KPI_Name
-    machineId = request.Machine_Name
-    machineType = request.Machine_Type
-    startPeriod = request.Date_Start
-    endPeriod = request.Date_End
-    aggregator = request.Aggregator
-    unitOfMeasure = 'UoM'
-    startPreviousPeriod = request.startPreviousPeriod
-    endPreviousPeriod = request.endPreviousPeriod
 
     # A list of all static KPI method calculation names is compiled for later use
     methods = {
@@ -77,26 +73,39 @@ async def calculate(request: KPIRequest, api_key: str = Depends(get_verify_api_k
     for name in dir(kpi_engine)
     if callable(getattr(kpi_engine, name)) and not name.startswith("__")
     }
-    
-    if(kpiID == "dynamic_kpi"):
-        raise HTTPException(status_code=404, detail=f"'dynamic_kpi' method not directly callable.")
 
-    # If the requested KPI is not in the static methods, call the dynamic KPI method. Otherwise, just call the good old static one
-    if kpiID not in methods:
-        result = kpi_engine.dynamic_kpi(df = df, machine_id = machineId, start_period = startPeriod, end_period = endPeriod, machine_type = machineType, kpi_id=kpiID)
-    else:
-        result = methods[kpiID](df = df, machine_id = machineId, machine_type=machineType, start_period = startPeriod, end_period = endPeriod, start_previous_period=startPreviousPeriod, end_previous_period=endPreviousPeriod)
+    def process_single_request(req: KPIRequest):
+        kpiID = req.KPI_Name
+        machineId = req.Machine_Name
+        machineType = req.Machine_Type
+        startPeriod = req.Date_Start
+        endPeriod = req.Date_Finish
+        aggregator = req.Aggregator
+        unitOfMeasure = 'UoM'
+        startPreviousPeriod = req.startPreviousPeriod
+        endPreviousPeriod = req.endPreviousPeriod
     
-    response = {
-        "Machine_Name": machineId,
-        "KPI_Name": kpiID,
-        "Value": result,
-        "Measure_Unit": machineType,
-        "Date_Start": startPeriod,
-        "Date_Finish": endPeriod,
-        "Aggregator": aggregator,
-        "Forecast": False
-    }
+        if(kpiID == "dynamic_kpi"):
+            raise HTTPException(status_code=404, detail=f"'dynamic_kpi' method not directly callable.")
+
+        # If the requested KPI is not in the static methods, call the dynamic KPI method. Otherwise, just call the good old static one
+        if kpiID not in methods:
+            result = kpi_engine.dynamic_kpi(df = df, machine_id = machineId, start_period = startPeriod, end_period = endPeriod, machine_type = machineType, kpi_id=kpiID)
+        else:
+            result = methods[kpiID](df = df, machine_id = machineId, machine_type=machineType, start_period = startPeriod, end_period = endPeriod, start_previous_period=startPreviousPeriod, end_previous_period=endPreviousPeriod)
+        
+        return {
+            "Machine_Name": machineId,
+            "KPI_Name": kpiID,
+            "Value": result,
+            "Measure_Unit": machineType,
+            "Date_Start": startPeriod,
+            "Date_Finish": endPeriod,
+            "Aggregator": aggregator,
+            "Forecast": False
+        }
+    
+    response = [process_single_request(req) for req in request]
     return response
 
 if __name__ == "__main__":
