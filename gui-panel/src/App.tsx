b86116544ec698bd48e79d4c46c71cdec80f8945
Alexthw46 <77152778+Alexthw46@users.noreply.github.com>
2024-12-08 17:07:49 +0100

    re-enable auth

--- gui-panel/src/App.tsx
+++ gui-panel/src/App.tsx
@@ -8,7 +8,7 @@ const App = () => {
 
     // User authentication state ---  set to true for development purposes
     //const [isAuthenticated, setIsAuthenticated] = useState(process.env.NODE_ENV === 'development');
-    const [isAuthenticated, setIsAuthenticated] = useState(true);
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
     const [userId, setUserId] = useState('');
     const [username, setUsername] = useState('Test User');
     const [token, setToken] = useState<string | null>(null);
