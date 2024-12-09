0f49d844261e2e79b2ced97c46b8e480747bd06e
shyimon <botticelli.cosimo@gmail.com>
2024-12-09 15:56:15 +0100

    Changes made to KB call

--- kpi-engine/src/kpi_calculation.py
+++ kpi-engine/src/kpi_calculation.py
@@ -396,8 +396,8 @@ class kpi_engine:
             "Content-Type": "application/json"
         }
         response = requests.get(f"http://kb:8000/kb/{kpi_id}/get_kpi", headers=headers)
-        formula = response.get("atomic_formula")
-        unit_of_measure = response.get("unit_measure")
+        formula = response["atomic_formula"]
+        unit_of_measure = response["unit_measure"]
         # formula = 'cycles_max'
         # unit_of_measure = '%'
         expr = parse_expr(formula)
