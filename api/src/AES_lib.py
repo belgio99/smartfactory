728314282fed86ef5f464e66bbe163437318ff1b
r.berni <r.berni@studenti.unipi.it>
2024-12-16 00:10:26 +0100

    Encryption of sensible data

--- api/src/AES_lib.py
+++ api/src/AES_lib.py
@@ -3,16 +3,25 @@ from Crypto.Util.Padding import pad, unpad
 from Crypto.Random import get_random_bytes
 import base64
 
-def encrypt_data(data:str, key):
+def encrypt_data(data:str, key:bytes):
     '''
-    
+    Args: 
+        data (str): string to encrypt
+        key (bytes): AES encryption key
+    Returns:
+        encrypted_data (str): string of encrypted data
     '''
     cipher = AES.new(key, AES.MODE_ECB) #use this mode 
     ciphertext = cipher.encrypt(pad(data.encode('utf-8'), AES.block_size))
     return base64.b64encode(ciphertext).decode('utf-8')
 
-def decrypt_data(data:bytes , key):
+def decrypt_data(data:str, key:bytes):
     '''
+    Args: 
+        data (str): string to encrypt
+        key (bytes): AES encryption key
+    Returns:
+        derypted_data (str): string of decrypted data
     '''
     cipher = AES.new(key, AES.MODE_ECB)
     decrypted_data = unpad(cipher.decrypt(base64.b64decode(data.encode('utf-8'))), AES.block_size)
