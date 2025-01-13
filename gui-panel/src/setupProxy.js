a48598ec1843c04ff83ee15838aaa34027423dcc
belgio99 <17623601+belgio99@users.noreply.github.com>
2025-01-13 13:39:45 +0100

    Fix wrong gui-panel proxy rewrite

--- gui-panel/src/setupProxy.js
+++ gui-panel/src/setupProxy.js
@@ -5,7 +5,7 @@ module.exports = function(app) {
     '/api', // Usa "/api" come path per indirizzare le richieste
     createProxyMiddleware({
       //target: 'http://localhost:10040',
-      target: 'http://smartfactory-api-1:8000',
+      target: 'http://api:8000',
       changeOrigin: true,
       pathRewrite: { '^/api': '' }, // Riscrive il path eliminando "/api" se necessario
     })
